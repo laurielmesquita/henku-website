be46b2fb960286ad4864fcc9d03ada55678fb0de
Lauriel Mesquita <laurielmesquita@me.com>
2021-01-23 21:14:18 -0300

    :mag: Improve SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Henku',
+        short_name: 'Henku',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
